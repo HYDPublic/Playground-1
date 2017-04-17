66d3d62e932ba38d6ff5d3b2f430b3b85598faeb
JanEggers <jan.Eggers@ferag.com>
2017-04-17 11:29:07 +0200

    odata working but meh

---
+++ Playground.core/Odata/PlaygroundModelBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Builder;
+using Microsoft.OData.Edm;
+using Playground.core.Models;
+
+namespace Playground.core.Odata
+{
+    public class PlaygroundModelBuilder
+    {
+        private readonly IAssemblyProvider m_assemblyProvider;
+        public PlaygroundModelBuilder(IAssemblyProvider assemblyProvider)
+        {
+            m_assemblyProvider = assemblyProvider;
+        }
+
+        public IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder(m_assemblyProvider);
+            builder.EntitySet<Company>(nameof(PlaygroundContext.Companies));
+            builder.EntitySet<Site>(nameof(PlaygroundContext.Sites));
+            
+            return builder.GetEdmModel();
+        }
+    }
+}
